ad8bb8c7fc0194c438be2709b60dbea988be58a9
rasel mridha <raselmridha792@gmail.com>
2025-01-23 17:23:48 +0600

    protecting the profile page if user is not loggedIn

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 const page = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+  if (!user) {
+    redirect("https://raselmridha792.kinde.com/auth/cx/_:nav&m:login&psid:019492e575492c16ffb7b58240553b8e");
+  }
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="space-y-3">
-        <h1 className="text-3xl">Hey {user.given_name} welcome to the dashboard</h1>
+        <h1 className="text-3xl">
+          Hey {user.given_name} welcome to the dashboard
+        </h1>
       </div>
     </div>
   );
